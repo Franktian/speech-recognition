691ad3cb7132a0ef5c1e4b78f8e064e6a4fe5d66
Franktian <tianyawen201209@hotmail.com>
2016-04-02 13:48:15 -0400

    Added some parameters initialization

--- gmmClassify.m
+++ gmmClassify.m
@@ -1,6 +1,13 @@
+% Initial parameters setup
 trainDir    = '/u/cs401/speechdata/Training';
 testDir     = '/u/cs401/speechdata/Testing';
 FD          = dir([testDir '/*.mfcc']);
+M           = 8;
+epsilon     = 0.01;
+max_iter    = 100;
+
+% Load the gmm model and log likehoods
+[gmms, Ls] = gmmTrain( trainDir, max_iter, epsilon, M );
 
 for i=1:length(FD)
     disp(FD(i).name);
