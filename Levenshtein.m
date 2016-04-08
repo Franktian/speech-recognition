218b83f34d33de168ef20f0778b08963459b5539
yuki <yukie.07@hotmail.com>
2016-04-07 20:24:01 -0400

    3.3, 4.1

--- Levenshtein.m
+++ Levenshtein.m
@@ -18,7 +18,8 @@ SE = 0;
 IE = 0;
 DE = 0;
 LEV_DIST = 0;
-word_count = 0;
+total_wcount = 0;
+output = fopen('3.3output.txt', 'w');
 
 hypo = fopen(hypothesis, 'r');
 hypoContent = textscan(hypo, '%d %d %s', 'delimiter','\n');
@@ -35,59 +36,13 @@ for i=1:length(hypoSens)
     oriAnnosen = annoContent{3};
     annosen = char(lower(regexprep(oriAnnosen, '[^a-zA-Z ]', '')));
     annoArray = strsplit(annosen);
-    word_count = word_count + length(annoArray);
+    
+    sent_wcount = length(annoArray);
+    total_wcount = total_wcount + sent_wcount;
     
     [sub, ins, del]=singleSentLevenshtein(hypoArray, annoArray);
     
-%     n = length(annoArray);
-%     m = length(hypoArray);
-%     R = zeros(n+1, m+1);
-%     B = zeros(n+1, m+1);
-%     R(:, 1) = inf;
-%     R(1, :) = inf;
-%     R(1, 1) = 0;
-%     for x=2:n+1
-%         for y=2:m+1
-%             del = R(x-1, y)+1;
-%             sub = R(x-1, y-1);
-%             if ~strcmp(annoArray(x-1), hypoArray(y-1))
-%                 sub = sub + 1;
-%             end
-%             ins = R(x, y-1) + 1;
-% 
-%             R(x, y) = min([del; sub; ins]);
-% 
-%             if R(x, y) == del
-%                 B(x, y) = UP;
-%             elseif R(x, y) == ins
-%                 B(x, y) = LEFT;
-%             else
-%                 B(x, y) = UP_LEFT;
-%             end
-%         end
-%     end
-% 
-%     del = 0;
-%     sub = 0;
-%     ins = 0;
-%     x = n+1;
-%     y = m+1;
-% 
-%     while x > 1 & y > 1
-%         if B(x, y) == UP
-%             del = del + 1;
-%             x = x - 1;
-%         elseif B(x, y) == LEFT
-%             ins = ins + 1;
-%             y = y - 1;
-%         else
-%             if ~strcmp(annoArray(x-1), hypoArray(y-1))
-%                 sub = sub + 1;
-%             end
-%             x = x - 1;
-%             y = y - 1;
-%         end
-%     end
+    fprintf(output, 'hypothesis sentence: %s\nsentence SE = %d, sentence IE = %d, sentence DE = %d, \n\n', hyposen, sub/sent_wcount, ins/sent_wcount, del/sent_wcount);
 
     SE = SE + sub;
     DE = DE + del;
@@ -99,11 +54,6 @@ SE = SE / word_count;
 DE = DE / word_count;
 IE = IE / word_count;
 LEV_DIST = SE + DE + IE;
-
-disp(SE);
-disp(DE);
-disp(IE);
-disp(LEV_DIST);
 
 end
 
--- ibmSpeech.m
+++ ibmSpeech.m
@@ -3,9 +3,25 @@ testDir      = '/u/cs401/speechdata/Testing/';
 FD           = dir(flacFiles);
 
 % Speech-to-text
+output = fopen('4.1output.txt', 'w');
 for i=1:length(FD)
     % Get the absolute file path
-    flac = [testDir '/' FD(i).name];
-    text = speechToText(flac);
-    disp(text);
+    flac = [testDir '/' 'unkn_' int2str(i) '.flac'];
+    text = char(speechToText(flac));
+    textArray = strsplit(lower(text));
+    
+    annoContent = textscan(fopen([testDir '/' 'unkn_' int2str(i) '.txt']), '%d %d %s', 'delimiter','\n');
+    oriAnnosen = annoContent{3};
+    annosen = char(lower(regexprep(oriAnnosen, '[^a-zA-Z ]', '')));
+    annoArray = strsplit(annosen);
+    word_count = length(annoArray);
+    
+    [se_count, ie_count, de_count]=singleSentLevenshtein(textArray, annoArray);
+    
+    sentSE = se_count / word_count;
+    sentIE = ie_count / word_count;
+    sentDE = de_count / word_count;
+    
+    fprintf(output, 'recognized transcript: %s\nSE = %d, IE = %d, DE = %d\n\n', text, annosen, sentSE, sentIE, sentDE);
+
 end
